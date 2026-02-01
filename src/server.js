0588b6a97363764d3b5a44a9f0c1338eeb3c148a
Hayk Hayrapetyan <hayk@tellonym.me>
2026-02-01 15:18:50 +0100

    remove js files and move to dist

--- src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var app_js_1 = require("./app.js");
-var port = process.env.PORT || 3000;
-app_js_1.app.listen(port, function () {
-    return console.log("Example app listening at http://localhost:".concat(port));
-});
